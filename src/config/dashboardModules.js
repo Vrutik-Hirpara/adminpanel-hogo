0108399021d7b1696378deb211029c6d557a4808
Vrutik Hirpara <vrutikhirpara01@gmail.com>
2026-01-30 15:31:41 +0530

    changes in files

--- src/config/dashboardModules.js
+++ src/config/dashboardModules.js
@@ -1,22 +1,65 @@
+// import { themes } from "./theme.config";
+// import { getDepartments } from "../services/department.service";
+// import { getRoles } from "../services/roles.service";
+// import { getOfficeBranches } from "../services/officebranches.service";
+
+// export const dashboardModules = [
+//   {
+//     title: "Departments",
+//     color: themes.cardDepartment,
+//     api: getDepartments,
+//   },
+//   {
+//     title: "Roles",
+//     color: themes.cardDonation,
+//     api: getRoles,
+//   },
+//   {
+//     title: "Office Branches",
+//     color: themes.cardEvent,
+//     api: getOfficeBranches,
+//   },
+// ];
+
+
+
+
+
+
+
+
+
+
 import { themes } from "./theme.config";
 import { getDepartments } from "../services/department.service";
 import { getRoles } from "../services/roles.service";
+import { getEmployees } from "../services/employee.service";
 import { getOfficeBranches } from "../services/officebranches.service";
 
 export const dashboardModules = [
   {
     title: "Departments",
-    color: themes.cardDepartment,
     api: getDepartments,
+    color: themes.cardDepartment,
+    path: "/department",
   },
   {
     title: "Roles",
-    color: themes.cardDonation,
     api: getRoles,
+    color: themes.cardDonation,
+    path: "/roles",
+  },
+    {
+    title: "Office Branches",
+    api: getOfficeBranches,
+    color: themes.cardEvent,
+    path: "/office-branches",
   },
   {
-    title: "Office Branches",
-    color: themes.cardEvent,
-    api: getOfficeBranches,
+    title: "Employees",
+    api: getEmployees,
+    color: themes.cardContact,
+    path: "/employee",
   },
+
 ];
