741946588fe0ab66c8e617c95ada8224c0ccd14a
Vrutik Hirpara <vrutikhirpara01@gmail.com>
2026-04-17 11:48:27 +0530

    travel plan changes

--- src/main.jsx
+++ src/main.jsx
@@ -3,19 +3,9 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
-<<<<<<< HEAD
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
       <App />
-=======
-import { DataProvider } from "./context/DataContext";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <DataProvider>
-      <App />
-    </DataProvider>
->>>>>>> 887504232e7413403b246f7238ba850bb75f8c89
   </BrowserRouter>
 );
